052f8e88f174d059f50ba1afa16463948527481f
glebfarmon <glebfarmon@gmail.com>
2026-02-27 15:07:43 +0100

    fixed safari experience

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,6 @@ import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// Lazy load fonts after initial render to prevent blocking
-// iOS Safari will use system fonts immediately, then swap when ready
-if (typeof window !== 'undefined') {
-	// Load fonts asynchronously after initial paint
-	requestIdleCallback(() => {
-		import('@fontsource/jetbrains-mono/400.css')
-		import('@fontsource/jetbrains-mono/700.css')
-		import('@fontsource/poppins/400.css')
-		import('@fontsource/poppins/600.css')
-		import('@fontsource/poppins/700.css')
-	}, { timeout: 2000 })
-}
-
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<App />
